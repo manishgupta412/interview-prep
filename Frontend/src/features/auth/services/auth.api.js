e87d1e1228a8394f46aa2d715381bf9b49018353
manishgupta412 <mguptag37@gmail.com>
2026-03-15 22:57:07 +0530

    fix auth cookies for production

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://interview-prep-mg7y.onrender.com",
+    baseURL: "http://localhost:3000",
     withCredentials: true
 })
 
